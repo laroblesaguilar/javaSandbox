4a9ef6a8b73ab5f253fd1f2a166e6c0d076f55e1
laroblesaguilar <laroblesaguilar@gmail.com>
2019-06-10 14:11:41 -0500

    greatestValue method

--- src/ArrayExercises.java
+++ src/ArrayExercises.java
@@ -1,47 +1,55 @@
 public class ArrayExercises {
 
-    static int[] bubbleSort(int [] num){
-        for(int i = 0; i < num.length; i++){
+    static int[] bubbleSort(int[] num) {
+        for (int i = 0; i < num.length; i++) {
             int greatestValue = 0;
-            for(int j = 0; j < num.length -1; j++){
-                if(num[j] > num[j + 1]){
+            for (int j = 0; j < num.length - 1; j++) {
+                if (num[j] > num[j + 1]) {
                     greatestValue = num[j];
-                    num[j] = num[j+1];
-                    num[j+1] = greatestValue;
+                    num[j] = num[j + 1];
+                    num[j + 1] = greatestValue;
                 }
             }
         }
         return num;
     }
 
-    static int[] reverseIntArray(int [] num){
-        int [] reversed = new int [num.length];
+    static int[] reverseIntArray(int[] num) {
+        int[] reversed = new int[num.length];
         int j = 0;
         int i = num.length - 1;
-        while(i >= 0){
-            reversed[j] = num [i];
+        while (i >= 0) {
+            reversed[j] = num[i];
             j++;
             i--;
         }
         return reversed;
     }
 
-    static int greatestValue(int [] num){
-
+    static int greatestValue(int[] num) {
+        int greatestValue = num[0];
+        for (int a : num) {
+            if (a > greatestValue) {
+                greatestValue = a;
+            }
+        }
+        return greatestValue;
     }
 
     public static void main(String[] args) {
 
-        int [] num = {7,5,6,2,1,0,8,9};
-        for(int a : bubbleSort(num)){
+        int[] num = {7, 5, 6, 2, 1, 100, 8, 9};
+        for (int a : bubbleSort(num)) {
             System.out.println(a);
         }
 
-        int [] num1 = {1,2,3,4,5,6};
+        int[] num1 = {1, 2, 3, 4, 5, 6};
 
-        for(int a : reverseIntArray(num1)){
+        for (int a : reverseIntArray(num1)) {
             System.out.println(a);
         }
+
+        System.out.println(greatestValue(num));
 
     }
 
